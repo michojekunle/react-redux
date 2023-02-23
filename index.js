36a5f72f7b556075ec409e001332a98048a362e2
AMD <michojekunle1@gmail.com>
2023-02-23 16:33:36 +0100

    made changes

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("From INDEX.JS");
+const redux =  require('redux');
+const createStore = redux.createStore;
+
+const BUY_CAKE = 'BUY_CAKE';
+
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: "FIRST REDUX ACTION"
+    }
+}
+
+// (previousState, action) => newState 
+
+const initialState = {
+    numberOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numberOfCakes: state.numberOfCakes - 1
+        }
+        default: return state
+    }
+}
+
+const store = createStore(reducer);
+console.log('Initial State: ', store.getState());
+const unsubscribe = store.subscribe(() => console.log('Updated State', store.getState()));
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe();

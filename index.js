b9755aa8e059a33fc29fe7cfb516218c7b971c73
michojekunle1@gmail.com <michojekunle1@gmail.com>
2023-02-26 16:48:23 +0100

    Working hmthriugh Spck Editor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux =  require('redux');
 const createStore = redux.createStore;
+const reducerCombiner = redux.reducerCombiner;
 
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -17,26 +18,56 @@ function buyIceCream() {
   }
 }
 
+const initialCakeStates = {
+  numberOfCakes: 10
+}
+
+const initialIceCreamStates = {
+  numberOfIceCreams: 20
+}
+
+////Single Reducers for multiple shopkeepers
 // (previousState, action) => newState 
+//const initialState = {
+//    numberOfCakes: 10, 
+//    numberOfIceCreams: 20
+//}
 
-const initialState = {
-    numberOfCakes: 10, 
-    numberOfIceCreams: 20
-}
+//const reducer = (state = initialState, action) => //{
+//  switch (action.type) {
+     //   case BUY_CAKE: return {
+         //   ...state,
+      //      numberOfCakes: state.numberOfCakes - 1
+      //  }
+        //case BUY_ICECREAM: return {
+         //   ...state,
+           // numberOfIceCreams: state.numberOfIceCreams - 1
+       // }
+        //default: return state
+   // }
+//}
 
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case BUY_CAKE: return {
-            ...state,
-            numberOfCakes: state.numberOfCakes - 1
-        }
-        case BUY_ICECREAM: return {
-            ...state,
-            numberOfIceCreams: state.numberOfIceCreams - 1
-        }
-        default: return state
+
+const cakeReducer = (state = initialCakeStates, action) => {
+  switch(action.type){
+    case BUY_CAKE: return {
+      ...state, 
+      numberOfCakes: state.numberOfCakes - 1
     }
+    default: return state 
+  }
 }
+
+const iceCreamReducer = (state = initialIceCreamStates, action) => {
+  switch(action.type){
+    case BUY_ICECREAM: return {
+      ...state, 
+      numberOfIceCreams: state.numberOfIceCreams - 1
+    }
+    default: return state 
+  }
+}
+
 
 const store = createStore(reducer);
 console.log('Initial State: ', store.getState());
